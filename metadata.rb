b39ae8cf93a5675904f3af2f93af957729db5519
Matt Mencel <mr-mencel@wiu.edu>
2014-10-27 17:12:23 -0500

    bugfix....foodcritic cleanup

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mr-mencel@wiu.edu'
 license 'Apache 2.0'
 description 'Installs/Configures rest2ldap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 supports 'ubuntu'
 
