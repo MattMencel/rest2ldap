bfb953641ce3ed48c9724aceef1ec09150a984bc
Matt Mencel <mr-mencel@wiu.edu>
2014-10-28 13:23:05 -0500

    update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'rest2ldap'
 maintainer 'Matt Mencel'
 maintainer_email 'mr-mencel@wiu.edu'
 license 'Apache 2.0'
-description 'Installs/Configures rest2ldap'
+description 'Installs/Configures the OpenDJ Rest2Ldap LDAP Gateway'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.3'
 
